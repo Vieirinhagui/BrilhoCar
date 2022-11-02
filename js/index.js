f3fa5b65ffd4e79ecb5daffa2b93c9690fef0338
Vieirinhagui <vieiragui147@gmail.com>
2022-11-02 20:30:49 -0300

    att

--- js/index.js
+++ js/index.js
@@ -9,9 +9,15 @@ $(document).ready(function(){
         $(".bnt-menu").css("display","flex");
         $(".bnt-menu").click(function(){
             $(".bnt-navbar-tell").css("display","flex");
+            $(".scroll").css("display","none");
         });
         $(".bnt-navbar-tell button").click(function(){
             $(".bnt-navbar-tell").css("display","none");
+            $(".scroll").css("display","block");
+        });
+        $(".bnt-navbar-tell a").click(function(){
+            $(".bnt-navbar-tell").css("display","none");
         });
     }
+    
 });
