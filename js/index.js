3cfde43ee8f5e90652fc89f4b465e8b99149f5af
Vieirinhagui <vieiragui147@gmail.com>
2022-11-02 18:42:34 -0300

    att

--- js/index.js
+++ js/index.js
@@ -2,41 +2,7 @@ $("#img-scroll").click(function(){
     $("html, body").animate({ scrollTop: 0 }, "slow");
 });
 
-
-
 $(document).ready(function(){
-    const windowwidth = $(document).width();
-    if(windowwidth<=450 && windowwidth>=390){
-        $("#img-menu").css("display","flex");
-        $(".bnt-navbar").css("display","none");
-        $("#img-menu").click(function(){
-            $("#img-menu").css("display","none");
-            $(".bnt-navbar").css("display","block");
-            $(".bnt-navbar").addClass("hide");
-        });
-        $("a").click(function(){
-            $("#img-menu").css("display","flex");
-            $(".bnt-navbar").css("display","none");
-        });
-    }
-});
-
-$(document).ready(function(){
-    const windowwidth = $(document).width();
-    if(windowwidth<=389 && windowwidth>=350 ){
-        $("#img-menu").css("display","flex");
-        $(".bnt-navbar").css("display","none");
-        $("#img-menu").click(function(){
-            $("#img-menu").css("display","none");
-            $(".bnt-navbar").css("display","block");
-            $(".bnt-navbar").addClass("hide");
-            $(".bnt-navbar.hide").css("margin","600px -10px 0 0");
-            $(".navbar-bnt").css("margin-right","0");
-        });
-        $("a").click(function(){
-            $("#img-menu").css("display","flex");
-            $(".bnt-navbar").css("display","none");
-        });
-    }
-   
+    windowwidth = $(document).width();
+    console.log(windowwidth);
 });
