7cf1e6ca6b258ec3b0465981d8df5e032f996d61
Vieirinhagui <vieiragui147@gmail.com>
2022-11-02 20:22:02 -0300

    att

--- js/index.js
+++ js/index.js
@@ -4,5 +4,14 @@ $("#img-scroll").click(function(){
 
 $(document).ready(function(){
     windowwidth = $(document).width();
-    console.log(windowwidth);
+    if (windowwidth<=420){
+        $(".bnt-navbar").css("display","none");
+        $(".bnt-menu").css("display","flex");
+        $(".bnt-menu").click(function(){
+            $(".bnt-navbar-tell").css("display","flex");
+        });
+        $(".bnt-navbar-tell button").click(function(){
+            $(".bnt-navbar-tell").css("display","none");
+        });
+    }
 });
